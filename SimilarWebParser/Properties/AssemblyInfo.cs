e65b02368d8e99ee8522554f8744937e16a4b50a
NullOne01 <NullOne01@users.noreply.github.com>
2021-10-13 04:16:52 +0300

    Added library description

--- SimilarWebParser/Properties/AssemblyInfo.cs
+++ SimilarWebParser/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimilarWebDataParser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Parser of a SimilarWeb data.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("NullOne")]
 [assembly: AssemblyProduct("SimilarWebDataParser")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
